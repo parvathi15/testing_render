311cd72d7fb5e4fd35624ffd25158c53e0dc8e95
parvathi15 <55029879+parvathi15@users.noreply.github.com>
2025-02-25 20:14:09 +0100

    added api

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Simple API route for testing
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
 // Start server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
